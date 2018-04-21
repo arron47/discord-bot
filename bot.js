4748e9272ca428882b92c79282dfbb131f1c6f22
arron47 <arron.reed47@gmail.com>
2018-04-21 14:25:27 -0400

    Testing auth token access through heroku

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 var Discord = require('discord.io');
 var logger = require('winston');
-var auth = require('./auth.json');
+var auth = process.env.auth;
 var text = require('./text.json');
 
 // Configure logger settings
