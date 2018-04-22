2bce0e8332a3bfd55055593caab6a96c8dea7f64
arron47 <arron.reed47@gmail.com>
2018-04-22 16:00:21 -0400

    Extremely simplistic case-insensitivity

--- bot.js
+++ bot.js
@@ -36,6 +36,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
+    message = message.toLowerCase();
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
