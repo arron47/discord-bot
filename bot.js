8d91292b8c059f8542ab8da52fcfc36f0aa624fc
arron47 <arron.reed47@gmail.com>
2018-04-21 23:02:37 -0400

    Testing small changes
    Wish I could hide some of these small changes. Testing locally and on
    the web simultaneously isn't great for commit count.

--- bot.js
+++ bot.js
@@ -30,8 +30,8 @@ bot.on('ready', function (evt) {
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Assign variables for the assignable roles
-    let speedRole = message.guild.roles.find("name", "Gotta Go Fsat");
-    let movieRole = message.guild.roles.find("name", "Movie Night");
+    var speedRole = message.guild.roles.find("name", "Gotta Go Fsat");
+    var movieRole = message.guild.roles.find("name", "Movie Night");
 
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
