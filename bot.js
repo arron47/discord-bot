1e989ed13b3f78f31e5081f225969ea378b5f77f
arron47 <arron.reed47@gmail.com>
2018-04-21 22:56:50 -0400

    Added roles commands
    Not sure if the add switch is going to work, need to test

--- bot.js
+++ bot.js
@@ -29,6 +29,10 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
+    // Assign variables for the assignable roles
+    let speedRole = message.guild.roles.find("name", "Gotta Go Fsat");
+    let movieRole = message.guild.roles.find("name", "Movie Night");
+
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
@@ -57,6 +61,46 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: parseJSON(text.commands)
                 });
+            break;
+            // !roles
+            case 'roles':
+                bot.sendMessage({
+                    to: channelID,
+                    message: parseJSON(text.roles)
+                });
+                switch(args[0]) {
+                    // !roles speed
+                    case 'speed':
+                        if(message.member.roles.has(speedRole)) {
+                            message.member.removeRole(speedRole).catch(console.error);
+                            bot.sendMessage({
+                                to: channelID,
+                                message: "You've been removed from the 'Gotta Go Fsat' role!"
+                            });
+                        } else {
+                            message.member.addRole(speedRole).catch(console.error);
+                            bot.sendMessage({
+                                to: channelID,
+                                message: "You're part of the 'Gotta Go Fsat' role now!"
+                            });
+                        }
+                    break;
+                    // !roles movie
+                    case 'movie':
+                    if(message.member.roles.has(movieRole)) {
+                        message.member.removeRole(movieRole).catch(console.error);
+                        bot.sendMessage({
+                            to: channelID,
+                            message: "You've been removed from the 'Movie Night' role!"
+                        });
+                    } else {
+                        message.member.addRole(movieRole).catch(console.error);
+                        bot.sendMessage({
+                            to: channelID,
+                            message: "You're part of the 'Movie Night' role now!"
+                        });
+                    }
+                }
             break;
             // Just add any case commands if you want to..
          }
